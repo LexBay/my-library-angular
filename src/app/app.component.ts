f011d378bc106e0f16576a6386c377c03dd9ea5f
Lex <lex.bay93@gmail.com>
2020-01-15 20:19:16 -0600

    added through step 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './book';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-library';
+
+  toRead = [
+    new Book("Notes from a Young Black Chef", "Kwame Onwauchi", 2019),
+    new Book("Salt, Fat, Acid, Heat", "Samin Nosrat", 2019)
+  ];
 }
